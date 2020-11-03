7e8dbcdc352478035481ccd55e20253679b4c043
ShabashovV <71067418+ShabashovV@users.noreply.github.com>
2020-11-03 13:08:39 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,9 @@
 import typing as tp
 
-
+up = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+             'V', 'W', 'X', 'Y', 'Z']
+low = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+             'v', 'w', 'x', 'y', 'z']
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -16,6 +19,15 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    for i in plaintext:
+        if i in up:
+            x = up.index(i)
+            ciphertext += up[(x + shift) % len(up)]
+        elif i in low:
+            x = low.index(i)
+            ciphertext += low[(x + shift) % len(low)]
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -34,8 +46,18 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if i in up:
+            x = up.index(i)
+            plaintext += up[(x - shift) % len(up)]
+        elif i in low:
+            x = low.index(i)
+            plaintext += low[(x - shift) % len(low)]
+        else:
+            plaintext += i
     return plaintext
-
+print(encrypt_caesar('PYTHON'))
+print(decrypt_caesar('SBWKRQ'))
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
@@ -44,3 +66,5 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
+
